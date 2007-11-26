51a87cc0cca07f58915d4ccbc028c53b181a2a77
ethomas <ethomas@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-11-26 11:02:54 +0000

    Import fixes

--- collective.passwordhistory/trunk/collective/passwordhistory/__init__.py
+++ collective.passwordhistory/trunk/collective/passwordhistory/__init__.py
@@ -1,15 +1,13 @@
 from Products.PluggableAuthService import registerMultiPlugin
 from AccessControl.Permissions import add_user_folders
 
-import patch
-import collective.passwordhistory.plugin
+import collective.passwordhistory.patch
+from collective.passwordhistory import plugin
 
 registerMultiPlugin(plugin.PasswordHistory.meta_type)
 
 def initialize(context):
-
     # Register PAS plug-in
-
     context.registerClass(plugin.PasswordHistory,
                           permission = add_user_folders,
                           constructors = (plugin.manage_addPasswordHistoryForm,
