9a5abc209d33e98bd7cb7b59548a237038f566ac
ethomas <ethomas@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-11-26 10:42:24 +0000

    Added some logging to aid debugging

--- collective.passwordhistory/trunk/collective/passwordhistory/storage.py
+++ collective.passwordhistory/trunk/collective/passwordhistory/storage.py
@@ -1,3 +1,5 @@
+import logging
+
 from zope.interface import implements
 
 from persistent import Persistent
@@ -5,6 +7,8 @@ from BTrees.OOBTree import OOBTree
 from AccessControl import AuthEncoding
 
 from collective.passwordhistory.interfaces import IUsedPasswordStorage
+
+log = logging.getLogger('collective.passwordhistory')
 
 class UsedPasswordStorage(Persistent):
     """A local utility for storing a list of previously used passwords.
@@ -18,24 +22,27 @@ class UsedPasswordStorage(Persistent):
     def isPasswordUsed(self, login, password, history_size=0):
         """Query password store to see if password has been previously used.
         """
-	for hash in self.getPasswordsForUser(login, history_size):
-	    if AuthEncoding.pw_validate(hash, password):
-	        return True
+        for hash in self.getPasswordsForUser(login, history_size):
+            if AuthEncoding.pw_validate(hash, password):
+                log.info("Password '%s' for user '%s' not valid (already used)" % (password, login))
+                return True
+        log.info("Password '%s' for user '%s' valid" % (password, login))
         return False
 
     def getPasswordsForUser(self, login, history_size=0):
         """Return a list of previously used paswords for a user.
         """
-	hashes = self._user_passwords.get(login, [])[-history_size:]
-	return hashes
+        hashes = self._user_passwords.get(login, [])[-history_size:]
+        return hashes
 
     def setPasswordForUser(self, login, password):
         """Add password to the list of previously used passwords for a user.
         """
-	hashes = self._user_passwords.get(login, [])
+        hashes = self._user_passwords.get(login, [])
         hash = AuthEncoding.pw_encrypt(password)
         hashes.append(hash)
         self._user_passwords[login] = hashes
+        log.info("Password '%s' for user '%s' stored" % (password, login))
 
     def clearPasswordsForUser(self, login):
         """Remove stored passwords for a user.
@@ -45,4 +52,4 @@ class UsedPasswordStorage(Persistent):
     def clearAllPasswords(self):
         """Remove stored passwords for all users.
         """
-	self._user_passwords.clear()
+        self._user_passwords.clear()
