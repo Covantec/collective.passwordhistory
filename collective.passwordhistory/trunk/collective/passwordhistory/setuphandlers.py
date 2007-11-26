ed42ccfe850de0f1382054489a754eb138a566fb
ethomas <ethomas@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-11-26 10:50:23 +0000

    Moved code from utils.py in here

--- collective.passwordhistory/trunk/collective/passwordhistory/setuphandlers.py
+++ collective.passwordhistory/trunk/collective/passwordhistory/setuphandlers.py
@@ -1,19 +1,20 @@
 from StringIO import StringIO
 
 from Products.CMFCore.utils import getToolByName
+from Products.PluggableAuthService.interfaces.plugins import IUserAdderPlugin
+from Products.PlonePAS.interfaces.plugins import IUserManagement
 from Products.PlonePAS.Extensions.Install import activatePluginInterfaces
 
-from collective.passwordhistory.config import PASSWORDHISTORY_PLUGIN_NAME
+from collective.passwordhistory.config import PLUGIN_NAME
 
 def importVarious(context):
-
     if context.readDataFile('collective.passwordhistory_various.txt') is None:
         return
   
     portal = context.getSite()
-    setup_passwordhistory_plugin(portal)
+    setupPlugin(portal)
 
-def setup_passwordhistory_plugin(portal):
+def setupPlugin(portal):
     """Install and prioritize the password history PAS plug-in
     """
     out = StringIO()
@@ -23,10 +24,12 @@ def setup_passwordhistory_plugin(portal):
     passwordhistory = uf.manage_addProduct['collective.passwordhistory']
     existing = uf.objectIds()
 
-    if PASSWORDHISTORY_PLUGIN_NAME not in existing:
-        passwordhistory.manage_addPasswordHistory(PASSWORDHISTORY_PLUGIN_NAME)
-        activatePluginInterfaces(portal, PASSWORDHISTORY_PLUGIN_NAME, out)
+    if PLUGIN_NAME not in existing:
+        passwordhistory.manage_addPasswordHistory(PLUGIN_NAME)
+        activatePluginInterfaces(portal, PLUGIN_NAME, out)
+	uf.plugins.movePluginsUp(IUserAdderPlugin, [PLUGIN_NAME])
+	uf.plugins.movePluginsUp(IUserManagement, [PLUGIN_NAME])
     else:
-        print >> out, "%s already installed" % PASSWORDHISTORY_PLUGIN_NAME
+        print >> out, "%s already installed" % PLUGIN_NAME
 
     return out.getvalue()
